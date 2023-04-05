fe5a6828bf6dedcd0ce46a3893d022eb9868370c
Jonathan Ngbonga <jonathan@ntls.io>
2023-04-06 02:42:46 +0300

    feat(pool): add file uploader

--- src/app/components/create-pool/create-pool.component.ts
+++ src/app/components/create-pool/create-pool.component.ts
@@ -1,33 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   NgWizardConfig,
   NgWizardService,
   StepChangedArgs,
   StepValidationArgs,
   STEP_STATE,
-  THEME,
-} from 'ng-wizard';
-import { of } from 'rxjs';
+  THEME
+} from "ng-wizard";
+import { FileUploader } from "ng2-file-upload";
+import { of } from "rxjs";
+
+const uploaderConfig = {
+  url: "",
+  disableMultipart: true,
+  allowedMimeType: ["application/json"]
+};
 
 @Component({
-  selector: 'app-create-pool',
-  templateUrl: './create-pool.component.html',
-  styleUrls: ['./create-pool.component.scss'],
+  selector: "app-create-pool",
+  templateUrl: "./create-pool.component.html",
+  styleUrls: ["./create-pool.component.scss"]
 })
 export class CreatePoolComponent implements OnInit {
   stepStates = {
     normal: STEP_STATE.normal,
     disabled: STEP_STATE.disabled,
     error: STEP_STATE.error,
-    hidden: STEP_STATE.hidden,
+    hidden: STEP_STATE.hidden
   };
 
   config: NgWizardConfig = {
     selected: 0,
-    theme: THEME.arrows,
+    theme: THEME.arrows
   };
 
   isValidTypeBoolean: boolean = true;
+
+  uploaderSchema = new FileUploader(uploaderConfig);
+  uploaderData = new FileUploader(uploaderConfig);
 
   constructor(private ngWizardService: NgWizardService) {}
 
