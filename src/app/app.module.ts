eef08b06b2533bbabeaddbdc7322742c3a2e3df5
Jonathan Ngbonga <jonathan@ntls.io>
2023-04-21 14:03:35 +0300

    build(plugin): add akita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
-import { NgModule } from '@angular/core';
+import {
+  ENVIRONMENT_INITIALIZER,
+  inject,
+  NgModule,
+  NgZone
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { akitaDevtools, DevtoolsOptions } from '@datorama/akita';
+import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { Store, StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { uiReducer } from './store/ui/reducer';
+
+export function provideAkitaDevtools(options: Partial<DevtoolsOptions> = {}) {
+  return {
+    provide: ENVIRONMENT_INITIALIZER,
+    multi: true,
+    useFactory() {
+      return () => {
+        akitaDevtools(inject(NgZone), options);
+      };
+    }
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,9 +35,19 @@ import { uiReducer } from './store/ui/reducer';
     AppRoutingModule,
     BrowserAnimationsModule,
     StoreModule.forRoot({ ui: uiReducer }),
-    SharedModule
+    SharedModule,
+    AkitaNgRouterStoreModule
   ],
-  providers: [Store],
+  providers: [
+    Store,
+    provideAkitaDevtools({
+      name: 'NTC'
+    }),
+    {
+      provide: NG_ENTITY_SERVICE_CONFIG,
+      useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
