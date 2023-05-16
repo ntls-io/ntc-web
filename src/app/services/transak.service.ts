14c103b85a1b37d74f61d0bfb029c407ef082956
Jonathan Ngbonga <jonathan@ntls.io>
2023-05-16 15:22:16 +0300

    refactor(transak): update settings on new user

--- src/app/services/transak.service.ts
+++ src/app/services/transak.service.ts
@@ -1,52 +1,49 @@
 import { Injectable } from '@angular/core';
-import Transak from '@transak/transak-sdk';
+import Transak, { Settings } from '@transak/transak-sdk';
 import { environment } from 'src/environments/environment';
 import { SessionQuery } from '../states/session';
+
+const { apiKey, cryptoCurrencyCode } = environment.transak;
+
+const transakSettings: Settings = {
+  apiKey,
+  cryptoCurrencyCode,
+  environment: environment.production ? 'PRODUCTION' : 'STAGING',
+  widgetHeight: '550px'
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransakService {
-  transak: Transak;
+  transak = new Transak(transakSettings);
 
   constructor(private sessionQuery: SessionQuery) {
-    const { vault } = this.sessionQuery.getValue();
-    const { apiKey, cryptoCurrencyCode } = environment.transak;
-
-    this.transak = new Transak({
-      apiKey,
-      cryptoCurrencyCode,
-      environment: environment.production ? 'PRODUCTION' : 'STAGING',
-      widgetHeight: '550px',
-      walletAddress: vault?.algorand_address_base32,
-      email: vault?.username
-    });
-
     this.listen();
   }
 
   listen() {
-    const { ALL_EVENTS, EVENTS } = this.transak;
+    const { EVENTS } = this.transak;
 
-    // To get all the events
-    this.transak.on(ALL_EVENTS, data => {
-      console.log(data);
-    });
-
-    // This will trigger when the user closed the widget
-    this.transak.on(EVENTS.TRANSAK_WIDGET_CLOSE, orderData => {
-      console.log(orderData);
+    this.transak.on(EVENTS.TRANSAK_WIDGET_CLOSE, () => {
       this.transak.close();
     });
 
-    // This will trigger when the user marks payment is made
-    this.transak.on(EVENTS.TRANSAK_ORDER_SUCCESSFUL, orderData => {
-      console.log(orderData);
+    this.transak.on(EVENTS.TRANSAK_ORDER_SUCCESSFUL, () => {
+      //TODO: handle successful order & update vault balance
       this.transak.close();
     });
   }
 
   open() {
+    const { vault } = this.sessionQuery.getValue();
+
+    this.transak = new Transak({
+      ...transakSettings,
+      walletAddress: vault?.algorand_address_base32,
+      email: vault?.username
+    });
+
     this.transak.init();
   }
 }
