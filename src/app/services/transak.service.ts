fc663bb34e393c41ade27dc2980b70abe16c14d9
Jonathan Ngbonga <jonathan@ntls.io>
2023-05-17 15:02:55 +0300

    feat(transak): fire notification on order success

--- src/app/services/transak.service.ts
+++ src/app/services/transak.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import Transak, { Settings } from '@transak/transak-sdk';
 import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
+import { AlgoService } from '../states/algo';
 import { SessionQuery } from '../states/session';
 
 const settings: Settings = environment.transak;
@@ -11,7 +13,10 @@ const settings: Settings = environment.transak;
 export class TransakService {
   transak = new Transak(settings);
 
-  constructor(private sessionQuery: SessionQuery) {
+  constructor(
+    private sessionQuery: SessionQuery,
+    private algoService: AlgoService
+  ) {
     this.listen();
   }
 
@@ -22,8 +27,12 @@ export class TransakService {
       this.transak.close();
     });
 
-    this.transak.on(EVENTS.TRANSAK_ORDER_SUCCESSFUL, () => {
-      //TODO: handle successful order & update vault balance
+    this.transak.on(EVENTS.TRANSAK_ORDER_SUCCESSFUL, ({ status }) => {
+      const text = status?.status.split('_').join(' ').toLowerCase();
+      Swal.fire({
+        icon: 'success',
+        text
+      }).finally(() => this.algoService.getAccountData());
       this.transak.close();
     });
   }
